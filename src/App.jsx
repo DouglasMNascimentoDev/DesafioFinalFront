1ad6fd1d0af2d9eb2adb6b429d6d459321d1d61e
DouglasMNascimentoDev <14.menezes@gmail.com>
2026-02-09 19:54:02 -0300

    Finalização das paginas de ações e quem somos.

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import NavBar from "./components/NavBar/NavBar"
 import Perfil from "./components/Perfil/Perfil"
+
 import "./global.scss"
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       
     <NavBar/>
     <Perfil/>
+    
     </>
   )
 }
