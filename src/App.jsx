dae68b49820601c2df25e2ca0e2e0730d0c46881
DouglasMNascimentoDev <14.menezes@gmail.com>
2026-02-11 19:08:15 -0300

    Finalização do desktop e responsivo do footer e página de início

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,16 @@
 import React from 'react'
-import NavBar from "./components/NavBar/NavBar"
-import Perfil from "./components/Perfil/Perfil"
+import NavBar from "./components/NavBar/NavBar";
+import Perfil from "./components/Perfil/Perfil";
+import Footer from "./components/Footer/Footer";
 
-import "./global.scss"
+import "./global.scss";
 function App() {
   return (
     <>
       
     <NavBar/>
     <Perfil/>
-    
+    <Footer/>
     </>
   )
 }
--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,10 +1,29 @@
-import React from 'react'
+import Logo from "../../assets/logo.png"
+import Whatsapp from "../../assets/whatsapp.png"
+import Instagram from "../../assets/instagram.png"
+import Linkedin from "../../assets/linkedin.png"
+import styles from "./Footer.module.scss"
 function Footer() {
   return (
-    <>
-    
-    <h2> Olá, boa noite! </h2>
-    </>
+    <footer>
+
+
+    <div>
+      <img src={Logo} alt="Logo da Connect" />
+      <h2>Connect</h2>
+    </div>
+
+    <p>
+      © 2026 Vai na Web & Empower. Todos os direitos reservados. Projeto destinado exclusivamente a fins educativos.
+    </p>
+
+    <nav>
+      <img src={Whatsapp} alt="Logo do Whatsapp" />
+      <img src={Instagram} alt="Logo do Instagram" />
+      <img src={Linkedin} alt="Logo do Linkedin" />
+    </nav>
+      
+    </footer>
   )
 }
 export default Footer
